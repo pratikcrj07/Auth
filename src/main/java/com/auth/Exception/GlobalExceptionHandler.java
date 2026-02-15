b963c7f382adc94b2e9f066b09cdb689449ea8e3
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-02-15 12:29:50 +0545

    feat(GlobalException Handler  impl )

---
+++ src/main/java/com/auth/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.auth.Exception;
+import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(
+            RuntimeException ex) {
+
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> error = new HashMap<>();
+        error.put("error",
+                ex.getBindingResult()
+                        .getFieldError()
+                        .getDefaultMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+    }
+}
